e2ec759f322c7b5c522b862cd7fb077d74806923
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-12-21 12:56:29 -0800

    add simple posts demo

---
+++ simple-posts-demo/src/features/posts/PostsList.js
@@ -0,0 +1,39 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+
+import {
+  selectAllPosts,
+  getPostsStatus,
+  getPostsError,
+  fetchPosts,
+} from "./postsSlice";
+import PostExcerpt from "./PostExcerpt";
+
+const PostsList = () => {
+  const dispatch = useDispatch();
+
+  const posts = useSelector(selectAllPosts);
+  const postStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
+
+  useEffect(() => {
+    if (postStatus === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [postStatus, dispatch]);
+
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {postStatus === "loading" && <p>Loading...</p>}
+      {postStatus === "failed" && <p>{error}</p>}
+      {postStatus === "succeeded" &&
+        orderedPosts.map((post) => <PostExcerpt key={post.id} post={post} />)}
+    </section>
+  );
+};
+export default PostsList;
