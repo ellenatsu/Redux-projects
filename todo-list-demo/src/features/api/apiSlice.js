8ca0f3de46435e58fa78e54680f3acf467a5d575
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-12-27 14:02:23 -0800

    finish todo list

--- todo-list-demo/src/features/api/apiSlice.js
+++ todo-list-demo/src/features/api/apiSlice.js
@@ -1,11 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+//any mutation result would get cached by default, have to invalidate the previous cache to show the updated data
 export const apiSlice = createApi({
   reducerPath: "api", // Unique key for reducer
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+  tagTypes: ["Todo"],
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => "/todos",
+      transformResponse: res => res.sort((a, b) => b.id - a.id), //sort the data by id in descending order
+      providesTags: ["Todo"],
     }),
     addTodo: builder.mutation({
       query: (todo) => ({
@@ -13,8 +17,30 @@ export const apiSlice = createApi({
         method: "POST",
         body: todo ,
       }),
+      invalidatesTags: ["Todo"],
+    }),
+    updateTodo: builder.mutation({
+      query: (todo) => ({
+        url: `/todos/${todo.id}`,
+        method: "PATCH",
+        body: todo,
+      }),
+      invalidatesTags: ["Todo"],
+    }),
+    deleteTodo: builder.mutation({
+      query: ({ id }) => ({
+        url: `/todos/${id}`,
+        method: "DELETE",
+        body: id
+      }),
+      invalidatesTags: ["Todo"],
     }),
   }),
 });
 
-export const { useGetTodosQuery } = apiSlice;
+export const { 
+  useGetTodosQuery,
+  useAddTodoMutation,
+  useUpdateTodoMutation,
+  useDeleteTodoMutation,
+ } = apiSlice;
--- todo-list-demo/src/features/todos/TodoList.js
+++ todo-list-demo/src/features/todos/TodoList.js
@@ -2,7 +2,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
-import { useGetTodosQuery } from "../api/apiSlice";
+import {
+  useGetTodosQuery,
+  useUpdateTodoMutation,
+  useAddTodoMutation,
+  useDeleteTodoMutation
+} from "../api/apiSlice";
 
 const TodoList = () => {
   const [newTodo, setNewTodo] = useState("");
@@ -14,41 +19,55 @@ const TodoList = () => {
     isError,
     error,
   } = useGetTodosQuery();
+  const [addTodo] = useAddTodoMutation()
+  const [updateTodo] = useUpdateTodoMutation()
+  const [deleteTodo] = useDeleteTodoMutation()
 
   const handleSubmit = (e) => {
     e.preventDefault();
     //addTodo
+    addTodo({ userId: 1, title: newTodo, completed: false })
     setNewTodo("");
   };
-
-  const newItemSection = (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="new-todo">Enter a new todo item</label>
-      <div className="new-todo">
-        <input
-          type="text"
-          id="new-todo"
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-          placeholder="Enter new todo"
-        />
-      </div>
-      <button className="submit">
-        <FontAwesomeIcon icon={faUpload} />
-      </button>
-    </form>
-  );
-
-  let content;
-  // Define conditional content
 
   return (
     <main>
       <h1>Todo List</h1>
-      {newItemSection}
-      {isSuccess && JSON.stringify(todos)}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="new-todo">Enter a new todo item</label>
+        <div className="new-todo">
+          <input
+            type="text"
+            id="new-todo"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            placeholder="Enter new todo"
+          />
+        </div>
+        <button className="submit">
+          <FontAwesomeIcon icon={faUpload} />
+        </button>
+      </form>
       {isLoading && <div>Loading...</div>}
-      {isError && <div>{error}</div>}
+      {isError && <p>{error}</p>}
+      {isSuccess && todos.map(todo => (
+        <article key={todo.id}>
+          <div className="todo">
+            <input
+              type="checkbox"
+              id={`${todo.id}`}
+              checked={todo.completed}
+              onChange={() => {
+                updateTodo({ ...todo, completed: !todo.completed })
+              }}
+            />
+            <label htmlFor={`${todo.id}`}>{todo.title}</label>
+          </div>
+          <button className="trash" onClick={() => deleteTodo({ id: todo.id })}>
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </article>
+      ))}
     </main>
   );
 };
